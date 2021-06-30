751e46b7524fc058159eadb74782275512071c43
Lucas Baradel <lbaradelmarchiori@gmail.com>
2021-06-29 22:47:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const handleRandom = () => {
+    var letters_numbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", 6, 7, 8, 9];
+  
+    var selectValues = letters_numbers.splice([Math.floor(Math.random() * letters_numbers.length)], 5);  
+    alert(selectValues);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 className="instructions">Clique no botão abaixo para gerar uma numeração aleatória de placa:</h3>
+      <div className="generate">
+        <button onClick={handleRandom}>Gerar</button>
+      </div>
+      <div className="rectangle">
+        <div class="rectangle-up">
+          Brasil
+        </div>
+      </div>
     </div>
   );
 }
