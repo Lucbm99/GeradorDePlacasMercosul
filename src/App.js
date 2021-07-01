03c8951d9e8a0574f81eaaf84c67709c9e1b1c93
Lucas Baradel <lbaradelmarchiori@gmail.com>
2021-06-30 23:40:07 -0300

    Fix - Exibição dos valores aleatórios dos vetores no console

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import './App.css';
 function App() {
 
   const handleRandom = () => {
-    var letters_numbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", 6, 7, 8, 9];
-  
-    var selectValues = letters_numbers.splice([Math.floor(Math.random() * letters_numbers.length)], 5);  
-    alert(selectValues);
+    var letters_position_1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var letters_position_2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var letters_position_3 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers_position_4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var numbers_position_5 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var letters_position_6 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers_position_7 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    var select_value_position_1 = letters_position_1[Math.floor(Math.random() * letters_position_1.length)];  
+    var select_value_position_2 = letters_position_2[Math.floor(Math.random() * letters_position_2.length)];  
+    var select_value_position_3 = letters_position_3[Math.floor(Math.random() * letters_position_3.length)];  
+    var select_value_position_4 = numbers_position_4[Math.floor(Math.random() * numbers_position_4.length)];  
+    var select_value_position_5 = numbers_position_5[Math.floor(Math.random() * numbers_position_5.length)];  
+    var select_value_position_6 = letters_position_6[Math.floor(Math.random() * letters_position_6.length)];  
+    var select_value_position_7 = numbers_position_7[Math.floor(Math.random() * numbers_position_7.length)]; 
+    
+    console.log(select_value_position_1);
+    console.log(select_value_position_2);
+    console.log(select_value_position_3);
+    console.log(select_value_position_4);
+    console.log(select_value_position_5);
+    console.log(select_value_position_6);
+    console.log(select_value_position_7);
+
   }
   
   return (
@@ -17,7 +37,7 @@ function App() {
         <button onClick={handleRandom}>Gerar</button>
       </div>
       <div className="rectangle">
-        <div class="rectangle-up">
+        <div className="rectangle-up">
           Brasil
         </div>
       </div>
