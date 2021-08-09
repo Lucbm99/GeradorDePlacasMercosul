799f682bb4692e1691f1fb65454d59372ffceeb9
Lucas Baradel <lbaradelmarchiori@gmail.com>
2021-08-09 20:54:36 -0300

    Fix - Refatoração dos estilos da aplicação

--- src/components/RectangleUp/index.js
+++ src/components/RectangleUp/index.js
@@ -5,6 +5,12 @@ export default function RectangleUp() {
     return (
         <div className="rectangle-up">
             Brasil
+            <div className="mercosul">   
+                <img src="/mercosul.png" alt="Ícone Mercosul" />
+            </div>
+            <div className="icon-brazil">
+                <img src="/brasil.png" alt="Ícone Brasil" />
+            </div>
         </div>
     )
 }
